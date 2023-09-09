0f74d048256d4eb48437cccb8029e414e31be5e9
Mariana <101410421+Placito@users.noreply.github.com>
2023-09-09 21:42:06 +0000

    adding navbar and footer

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -18,75 +18,89 @@ export const Navbar = (props) => {
 
 				return (
 					<nav className="navbar navbar-dark navbar-expand-lg fixed-top">
-					<div className="container-fluid d-flex">
-						<figure className="col-sm-4 col-md-3 col-lg-1 col-xl-1">
-						<img src={logo} className="img" alt="logo" />
-						</figure>
-						<div className="menu col-sm-8 col-md-12 col-lg-12 col-xl-12">
-						<div className="mb-3 language">
-							<button className="btnlanguage">English - Euro <i className="fa-solid fa-chevron-down"></i></button>
-						</div>
-						<button onClick={() => { toggleAdditionalButtons(); hideButton(); }} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-							<span className="navbar-toggler-icon"></span>
-						</button>
-						<div className={`collapse navbar-collapse justify-content-end ${showAdditionalButtons ? "active" : ""}`} id="navbarNavAltMarkup">
-							<div className="navbar-nav">
-							<Link className="link" to="/">
-								<button className="btn" href="#">Contact Us</button>
-							</Link>
-							<Link className="link" to="/">
-								<button className="btn" href="#">About Us</button>
-							</Link>
-							<div className="link dropdown">
-								<button className="btn dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-									Products
+						<div className="container-fluid d-flex">
+							<figure className="col-sm-4 col-md-3 col-lg-1 col-xl-1">
+							<img src={logo} className="img" alt="logo" />
+							</figure>
+							<div className="menu col-sm-8 col-md-12 col-lg-12 col-xl-12">
+							<div class="dropdown mb-3 language">
+								<button className="btnlanguage dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+									English - Euro <i className="fa-solid fa-chevron-down"></i>
 								</button>
-									<ul className="dropdown-menu">
-										<li><button className="dropdown-item" href="#">Road Bikes</button></li>
-										<li><button className="dropdown-item" href="#">Mountain Bikes</button></li>
-										<li><button className="dropdown-item" href="#">Hybrid Bikes</button></li>
-										<li><button className="dropdown-item" href="#">Speciality Bikes</button></li>
-										<li><button className="dropdown-item" href="#">BMX Bikes</button></li>
-										<li><button className="dropdown-item" href="#">Kid's Bikes</button></li>
-										<li><button className="dropdown-item" href="#">Other types</button></li>
-									</ul>
+								<ul className="dropdown-language dropdown-menu">
+										<li>
+											<label  className="form-label">Language</label>
+											<input type="text" className="form-control mb-3" id="exampleFormControlInput1" />
+										</li>
+										<li>
+											<label className="form-label">Currency</label>
+											<input type="text" className="form-control" id="exampleFormControlInput1" />
+										</li>
+									<br /> 
+									<button className="form-btn" href="#">APPLY</button>
+								</ul>
 							</div>
-							{hideButtons && (
-								<>
+							<button onClick={() => { toggleAdditionalButtons(); hideButton(); }} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+								<span className="navbar-toggler-icon"></span>
+							</button>
+							<div className={`collapse navbar-collapse justify-content-end ${showAdditionalButtons ? "active" : ""}`} id="navbarNavAltMarkup">
+								<div className="navbar-nav">
 								<Link className="link" to="/">
-								<i className="icon fa-solid fa-magnifying-glass"></i>
-							</Link>
-							<Link className="link" to="/">
-								<i className="icon fa-regular fa-user"></i>
-							</Link>
-							<Link className="link" to="/">
-								<i className="icon fa-solid fa-cart-shopping" tabIndex="-1" ></i>
-							</Link>
-							</>
-							)}
-							{showAdditionalButtons && (
-								<>
-								<Link className="link" to="/">
-									<i className="icon fa-solid fa-magnifying-glass"></i> Search
+									<button className="btn" href="#">Contact Us</button>
 								</Link>
 								<Link className="link" to="/">
-									Login
+									<button className="btn" href="#">About Us</button>
+								</Link>
+								<div className="link dropdown">
+									<button className="btn dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" >
+										Products
+									</button>
+										<ul className="dropdown-menu">
+											<li><button className="dropdown-item" href="#">Road Bikes</button></li>
+											<li><button className="dropdown-item" href="#">Mountain Bikes</button></li>
+											<li><button className="dropdown-item" href="#">Hybrid Bikes</button></li>
+											<li><button className="dropdown-item" href="#">Speciality Bikes</button></li>
+											<li><button className="dropdown-item" href="#">BMX Bikes</button></li>
+											<li><button className="dropdown-item" href="#">Kid's Bikes</button></li>
+											<li><button className="dropdown-item" href="#">Other types</button></li>
+										</ul>
+								</div>
+								{hideButtons && (
+									<>
+									<Link className="link" to="/">
+									<i className="icon fa-solid fa-magnifying-glass"></i>
 								</Link>
 								<Link className="link" to="/">
-									Register
+									<i className="icon fa-regular fa-user"></i>
 								</Link>
 								<Link className="link" to="/">
-									<i className="icon fa-solid fa-cart-shopping" tabIndex="-1"></i> Cart
-								</Link>
-								<Link className="link" to="/">	
-									<p>Give us your feedback</p>
+									<i className="icon fa-solid fa-cart-shopping" tabIndex="-1" ></i>
 								</Link>
 								</>
-							)}
+								)}
+								{showAdditionalButtons && (
+									<>
+									<Link className="link" to="/">
+										<i className="icon fa-solid fa-magnifying-glass"></i> Search
+									</Link>
+									<Link className="link" to="/">
+										Login
+									</Link>
+									<Link className="link" to="/">
+										Register
+									</Link>
+									<Link className="link" to="/">
+										<i className="icon fa-solid fa-cart-shopping" tabIndex="-1"></i> Cart
+									</Link>
+									<Link className="link" to="/">	
+										<p>Give us your feedback</p>
+									</Link>
+									</>
+								)}
+								</div>
+								</div>
 							</div>
 						</div>
-						</div>
-					</div>
-				</nav>
+					</nav>
 				);
 				};
