d0c08372a19e4a5f7cd723477d26fb6d3a575346
Mariana <101410421+Placito@users.noreply.github.com>
2023-09-09 19:57:34 +0000

    adding Navbar and Footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,23 +3,24 @@ import { Link } from "react-router-dom";
 import "../../styles/footer.css";
 
 export const Footer = () => (
-	<div className="container-fluid p-2 fixed-bottom d-flex">
+	
+	<div className="container-fluid fixed-bottom d-flex">
 		<div className="col-sm-8 col-md-8 col-lg-9">
-			<div className="d-flex">
-				<div className="m-3">
-					<i className="fa-regular fa-copyright mt-3"></i>
+			<div className="develop">
+				<div>
+					<i className="fa-regular fa-copyright"></i>
 				</div>
-				<div className="mt-3">
+				<div>
 					<p>Develop by Aleksander Klenin/ Mariana Plácito</p>
 					<p>4Geeks 2023</p>
 				</div>
 			</div>	
 		</div>
 		<div className="footerLinks col-sm-4 col-md-4 col-lg-3 d-flex">
-			<Link className="link mt-4" to="/">	
+			<Link className="link" to="/">	
 				<p><i className="fa-regular fa-comment-dots"></i></p>
 			</Link>
-			<Link className="link mt-4" to="/">	
+			<Link className="link" to="/">	
 				<p className="terms">terms & conditions</p>
 			</Link>
 		</div>	
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,31 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../img/logo.png";
 import "../../styles/navbar.css";
 
-export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-dark navbar-expand-lg fixed-top ">
-        	<div className="container-fluid ">
-				<figure>
-		  			<img src={logo} className="img" alt="logo" /> 
-				</figure>
-				<div>
-					<div className="mb-3">
-						<button className="language">English - Euro <i className="fa-solid fa-chevron-down"></i></button>
-					</div>
-            		<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            			<span className="navbar-toggler-icon"></span>
-           			</button>
-            		<div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-            			<div className="navbar-nav">
-							<Link to="/">
+export const Navbar = (props) => {
+  const [showAdditionalButtons, setShowAdditionalButtons] = useState(false);
+  const [hideButtons, sethideButtons] = useState(true);
+
+  // Function to toggle the display of additional buttons
+  const toggleAdditionalButtons = () => {
+    setShowAdditionalButtons(!showAdditionalButtons);
+  };
+  // Function to hide the previous buttons
+  const hideButton = () => {
+    sethideButtons(!hideButtons);
+  };
+
+				return (
+					<nav className="navbar navbar-dark navbar-expand-lg fixed-top">
+					<div className="container-fluid d-flex">
+						<figure className="col-sm-4 col-md-3 col-lg-1 col-xl-1">
+						<img src={logo} className="img" alt="logo" />
+						</figure>
+						<div className="menu col-sm-8 col-md-12 col-lg-12 col-xl-12">
+						<div className="mb-3 language">
+							<button className="btnlanguage">English - Euro <i className="fa-solid fa-chevron-down"></i></button>
+						</div>
+						<button onClick={() => { toggleAdditionalButtons(); hideButton(); }} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+							<span className="navbar-toggler-icon"></span>
+						</button>
+						<div className={`collapse navbar-collapse justify-content-end ${showAdditionalButtons ? "active" : ""}`} id="navbarNavAltMarkup">
+							<div className="navbar-nav">
+							<Link className="link" to="/">
 								<button className="btn" href="#">Contact Us</button>
 							</Link>
-							<Link to="/">
-								<button className="btn ms-3" href="#">About Us</button>
+							<Link className="link" to="/">
+								<button className="btn" href="#">About Us</button>
 							</Link>
-							<div className="dropdown ms-3">
+							<div className="link dropdown">
 								<button className="btn dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 									Products
 								</button>
@@ -39,19 +51,42 @@ export const Navbar = () => {
 										<li><button className="dropdown-item" href="#">Other types</button></li>
 									</ul>
 							</div>
-							<Link to="/">
-								<i className="icon fa-solid fa-magnifying-glass ms-3"></i>
+							{hideButtons && (
+								<>
+								<Link className="link" to="/">
+								<i className="icon fa-solid fa-magnifying-glass"></i>
 							</Link>
-							<Link to="/">
-								<i className="icon fa-regular fa-user ms-3"></i>
+							<Link className="link" to="/">
+								<i className="icon fa-regular fa-user"></i>
 							</Link>
-							<Link to="/">
-								<i className="icon fa-solid fa-cart-shopping ms-3" tabIndex="-1" ></i>
+							<Link className="link" to="/">
+								<i className="icon fa-solid fa-cart-shopping" tabIndex="-1" ></i>
 							</Link>
-            			</div>
-        			</div>
-    			</div>
-			</div>
-		</nav>
-	);
-};
+							</>
+							)}
+							{showAdditionalButtons && (
+								<>
+								<Link className="link" to="/">
+									<i className="icon fa-solid fa-magnifying-glass"></i> Search
+								</Link>
+								<Link className="link" to="/">
+									Login
+								</Link>
+								<Link className="link" to="/">
+									Register
+								</Link>
+								<Link className="link" to="/">
+									<i className="icon fa-solid fa-cart-shopping" tabIndex="-1"></i> Cart
+								</Link>
+								<Link className="link" to="/">	
+									<p>Give us your feedback</p>
+								</Link>
+								</>
+							)}
+							</div>
+						</div>
+						</div>
+					</div>
+				</nav>
+				);
+				};
