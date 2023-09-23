532947351ed0cad8f38bd689d0dc9cb95f6c3a42
Mariana <101410421+Placito@users.noreply.github.com>
2023-09-23 23:00:39 +0000

    check reset

--- src/api/config.py
+++ src/api/config.py
@@ -7,7 +7,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'fallback-jwt-secret-key')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
-<<<<<<< HEAD
     # SMTP credentials
     MAIL_SERVER = 'sandbox.smtp.mailtrap.io'
     MAIL_PORT = 2525
@@ -15,5 +14,3 @@ class Config:
     MAIL_PASSWORD = '562e49068d33bb'
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-=======
->>>>>>> 80ff9f8a7ca9eecf13cf7987ee228d77a7e7cad8
--- src/front/js/pages/resetPassword.js
+++ src/front/js/pages/resetPassword.js
@@ -9,46 +9,10 @@ export const ResetPassword = () => {
   const [hideContainer, setHideContainer] = useState(true);
   const [resetStatus, setResetStatus] = useState(null);
 
-<<<<<<< HEAD
   //function to send the email to reset password 
 const handleResetPassword = async () => {
   const data = {
     email: email
-=======
-  const handleResetPassword = async () => {
-    const data = {
-      email: email, // You can also use shorthand: email
-    };
-  
-    const opts = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: JSON.stringify(data), // Use the 'data' option for the JSON payload
-    };
-  
-    console.log(opts);
-  
-    try {
-      // Send a POST request to your server to initiate the password reset process
-      const response = await axios.post(process.env.BACKEND_URL + "/resetPassword", opts);
-      console.log("ok");
-  
-      if (response.status === 200) {
-        setResetStatus("Check your email for a password reset!");
-      } else if (response.status === 404) {
-        setResetStatus("Your email does not exist.");
-      } else {
-        setResetStatus("Error sending reset email.");
-      }
-    } catch (error) {
-      console.error("Error sending reset email:", error);
-      setResetStatus("Error sending reset email.");
-    }
-  
-    setHideContainer(false);
->>>>>>> 80ff9f8a7ca9eecf13cf7987ee228d77a7e7cad8
   };
 
   const opts = {
@@ -83,54 +47,6 @@ const handleResetPassword = async () => {
 
 
   return (
-<<<<<<< HEAD
-        <div className="min-height-100 container reset-big-box">
-         {/* Modal */}
-          {resetStatus &&  (
-                  <div className="my-modal">
-                    <img src={logo} className="logo" alt="logo" />
-                    <p>{resetStatus}</p>
-                  </div>
-          )}
-          {hideContainer && (
-            <>
-              <div>
-              <br />
-                <h1 className="reset-title">Reset account password</h1>
-                </div>
-                <br />
-                <div className="reset-box">
-                  <div>
-                    <p className="paragraf">Forgot your password? Enter your email.</p>
-                    <p className="paragraf">
-                      You will receive a link in your email to reset your password.
-                    </p>
-                    <br />
-                    <br />
-                    <input
-                      className="control reset-input"
-                      type="text"
-                      placeholder="Email"
-                      aria-label="default input example"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)} />
-                  </div>
-                  <br />
-                  <br />
-                  <br />
-                  <div>
-                    {/* Button trigger modal and send email */}
-                    <button
-                      onClick={handleResetPassword}
-                      className="btn-send"
-                    >
-                      Send
-                    </button>
-                  </div>
-                </div>
-            </>
-            )}
-=======
     <div className="min-height-100 container reset-big-box">
       {/* Modal */}
       {resetStatus && (
@@ -162,7 +78,6 @@ const handleResetPassword = async () => {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
->>>>>>> 80ff9f8a7ca9eecf13cf7987ee228d77a7e7cad8
             </div>
             <br />
             <br />
