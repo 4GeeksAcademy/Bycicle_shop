5efa138a0d8b03ed5109328da78f39b3c0ca2155
Aleksandr Klenin <59577486+Sol-max@users.noreply.github.com>
2023-09-24 15:47:06 +0000

    Products type is now shown

---
+++ src/front/js/TypeContext.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SelectedTypeContext = React.createContext();
+
+export default SelectedTypeContext;
--- src/front/js/pages/products.js
+++ src/front/js/pages/products.js
@@ -1,32 +1,63 @@
-import React, { useState, useEffect } from "react"; 
+import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import SelectedTypeContext from "../TypeContext";
 
 function Product(props) {
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
-  
+  const { selectedType } = useContext(SelectedTypeContext);
+
   function getData() {
     console.log("getData called");
-    axios
-      .get(process.env.BACKEND_URL + "/api/products")  
-      .then((response) => {
-        console.log("Response received: ", response.data); 
-        if (response.data.success === "true") {
-          setProducts(response.data.bicycles);
-          console.log("Fetched products: ", response.data.bicycles);
-        }
-      })
+
+
+    let url = process.env.BACKEND_URL + "/api/products";
+    if (selectedType) {
+      axios
+        .get(url)
+        .then((response) => {
+          console.log("Response received: ", response.data);
+          if (response.data.success === "true") {
+            const filteredProducts = response.data.bicycles.filter((product) =>
+              product.name.includes(selectedType)
+            );
+            setProducts(filteredProducts);
+            console.log("Fetched products: ", filteredProducts);
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+        });
+    } else {
+      axios
+        .get(url)
+        .then((response) => {
+          console.log("Response received: ", response.data);
+          if (response.data.success === "true") {
+            setProducts(response.data.bicycles);
+            console.log("Fetched products: ", response.data.bicycles);
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+        });
+    }
   }
-  
+
   useEffect(() => {
-    console.log("useEffect called"); 
+    getData(); // fetch the products when selectedType is changed
+  }, [selectedType]);
+
+  useEffect(() => {
+    console.log("useEffect called");
     if (!props.token && props.token !== "" && props.token !== undefined) {
       navigate("/login");
     }
     getData();
-  }, []);
-  
+  }, [selectedType]);
+
+
   return (
     <div>
       <h1>Product List</h1>
