d08cf44086b52a6418bb933d39698eadd760239e
Mariana <101410421+Placito@users.noreply.github.com>
2023-09-25 16:18:03 +0000

    change SMTP credentials

--- src/api/config.py
+++ src/api/config.py
@@ -11,6 +11,6 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USERNAME = 'mariana.placito@gmail.com'
-    MAIL_PASSWORD = '912479259'
+    MAIL_PASSWORD = 'Mp.3558p'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
