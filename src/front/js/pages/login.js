42a22441cdcd1d319d7796fc181358934ee5b003
Aleksandr Klenin <59577486+Sol-max@users.noreply.github.com>
2023-10-11 15:26:55 +0000

    google id added

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -120,7 +120,7 @@ export function Login(props) {
               <div className="container my-login">
                 <div className="btn-login-google">
                   <GoogleLogin
-                    clientId="YOUR_GOOGLE_CLIENT_ID"
+                    clientId="1024574470833-2aouoemahgn91lk4g61j4qfkvukigan0.apps.googleusercontent.com"
                     buttonText="LOGIN WITH GOOGLE "
                     onSuccess={responseGoogleSuccess}
                     onFailure={responseGoogleFailure}
